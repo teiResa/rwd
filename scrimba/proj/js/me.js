e0fe23d2c7a7e9f88173a7251532228594d17e66
Theresa Balsdon <resablsdn@outlook.com>
2024-06-19 22:00:01 +0800

    test

--- scrimba/proj/js/me.js
+++ scrimba/proj/js/me.js
@@ -1,3 +1,5 @@
+/*
+
 let mySkills = ["Web Development", "Arduino", "UX/UI Design", "Graphic Design", "Photography", ]
 let skillEl = document.getElementById("skills-el")
 let myExperience = ["Frontend Development", "Brand Manager", "Graphic Design",  "Teacher", "Leader", "Former Pastry Chef", "Caterer", "Sales", "Customer Service"]
@@ -7,7 +9,7 @@ let experienceEl = document.getElementById("experience-el")
 
 skillEl.textContent = mySkills //+ mySkills[1]
 experienceEl.textContent = myExperience
-
+*/
 /*
 console.log(mySkills)
 
@@ -15,7 +17,7 @@ console.log(myExperience[1])
 console.log(myExperience[2])
 console.log(myExperience[0])
 */
-
+/*
 //i forgot to use []
 let theresa = ["Theresa Dutrisac", 29, true]
 
@@ -37,13 +39,15 @@ console.log(messages)
 
 messages.pop() //don't have to specify, i guess it takes off the last item??
 console.log(messages)
-
+*/
 //Counting in javascript
 //Specify: 1)where to start, 2)where to stop, 3) shat is the step size (by ones, twos, tens, etc)
 
+
 //        START         STOP         STEP
-for (let count = 1; count < 11; count =+1){
+for ( let count = 1; count < 11; count += 1 ){
  //starting at 1, stop before 11 (runs ten times), counting by ones
- 
+ console.log(count)
 }
 
+//console.log("hey beech, hey")
