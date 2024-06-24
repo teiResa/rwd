d832deb4b4bc316911a4bc7c07ce6caa6d2f8bff
Theresa Balsdon <resablsdn@outlook.com>
2024-06-24 22:50:33 +0800

    07 - 01

---
+++ scrimba/proj/js/leads.js
@@ -0,0 +1,13 @@
+let inputEl = document.getElementById("input-el")
+let myLeads = []
+
+
+/* Is this amature hour? Use the element's id 
+function input() {
+ console.log("inputEl clicked")
+} */
+
+let inputBtn = document.getElementById("input-btn").addEventListener("click", function(){
+ console.log("event listener button")
+})
+
