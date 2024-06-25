d47aa079c5bf7d542f69576863119e8feae65a72
Theresa Balsdon <resablsdn@outlook.com>
2024-06-25 23:34:29 +0800

    Update leads.js

--- scrimba/proj/js/leads.js
+++ scrimba/proj/js/leads.js
@@ -5,6 +5,8 @@ let myLeads = []
 /* Is this amature hour? Use the element's id 
 function input() {
  console.log("inputEl clicked")
+
+ this is code. I am coding.
 } */
 
 let inputBtn = document.getElementById("input-btn").addEventListener("click", function(){
