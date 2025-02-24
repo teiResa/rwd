09d80a8f00b528efa5e9c26f269cd40acbf4e6b2
Theresa Balsdon <resablsdn@outlook.com>
2025-02-24 15:48:32 +0800

    reviewing the scrimba to fiunally finish the course

---
+++ scrimba/proj/js/quickies.js
@@ -0,0 +1,15 @@
+console.log("hello world johny apple seeds and almonds")
+
+
+
+ /* 2025-02-24 Dog Age Calculator */
+ let myAge =  29
+ let humanDogRatio = 7
+ let output = document.getElementById("dogAgeHere")
+
+ let myDogAge = myAge * humanDogRatio
+
+ output.innerHTML("myDogAge")
+
+ console.log("myDogAge")
+ 
